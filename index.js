def654566808d7c871bd90f79e0a44d96adcc511
radahn-in <raghu_nath1993@yahoo.com>
2023-09-27 16:44:38 +0530

    random hover effect added

--- index.js
+++ index.js
@@ -3,10 +3,43 @@ const container = document.querySelector('#container');
 const numRows = 16;
 const numCols = 16;
 
+
 for (let i = 0; i< (numRows * numCols); i++){
-    const gridItem = document.createElement('div');
-    gridItem.classList.add('grid-item');
-    gridItem.textContent = i + 1;
-    container.appendChild(gridItem);
+    const gridItems = document.createElement('div');
+    gridItems.classList.add('grid-item');
+    gridItems.textContent = i + 1;
+
+    gridItems.addEventListener('mouseover', function() {
+        const randomColor = getRandomColor();
+        gridItems.style.backgroundColor = randomColor;
+    });
+
+    gridItems.addEventListener('mouseout', function() {
+        gridItems.style.backgroundColor = 'white';
+    })
+
+
+
+    container.appendChild(gridItems);
 }
+
+function getRandomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r},${g},${b})`;
+}
+
+// const gridItems = document.querySelectorAll('grid-item');
+
+// gridItems.forEach((item) => {
+//     item.addEventListener('mouseover', function() {
+//         const randomColor = getRandomColor();
+//         item.style.backgroundColor = randomColor;
+//     });
+
+//     item.addEventListener('mouseout',function(){
+//         item.style.backgroundColor = 'white';
+//     });
+// });
 
