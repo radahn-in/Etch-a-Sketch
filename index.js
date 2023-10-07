b1e1601a16623bea93004ad3c059e1ffe50c6427
radahn-in <raghu_nath1993@yahoo.com>
2023-10-07 20:05:28 +0530

    added hovering with left click

--- index.js
+++ index.js
@@ -1,28 +1,3 @@
-const container = document.querySelector('#container');
-
-const numRows = 16;
-const numCols = 16;
-
-
-for (let i = 0; i< (numRows * numCols); i++){
-    const gridItems = document.createElement('div');
-    gridItems.classList.add('grid-item');
-    gridItems.textContent = i + 1;
-
-    gridItems.addEventListener('mouseover', function() {
-        const randomColor = getRandomColor();
-        gridItems.style.backgroundColor = randomColor;
-    });
-
-    gridItems.addEventListener('mouseout', function() {
-        gridItems.style.backgroundColor = 'white';
-    })
-
-
-
-    container.appendChild(gridItems);
-}
-
 function getRandomColor() {
     const r = Math.floor(Math.random() * 256);
     const g = Math.floor(Math.random() * 256);
@@ -30,16 +5,40 @@ function getRandomColor() {
     return `rgb(${r},${g},${b})`;
 }
 
-// const gridItems = document.querySelectorAll('grid-item');
+const container = document.querySelector('#container');
 
-// gridItems.forEach((item) => {
-//     item.addEventListener('mouseover', function() {
-//         const randomColor = getRandomColor();
-//         item.style.backgroundColor = randomColor;
-//     });
+const numRows = 16;
+const numCols = 16;
 
-//     item.addEventListener('mouseout',function(){
-//         item.style.backgroundColor = 'white';
-//     });
-// });
+let isHovering = false;
+
+for (let i = 0; i< (numRows * numCols); i++){
+    const gridItems = document.createElement('div');
+    gridItems.classList.add('grid-item');
+    
+
+    gridItems.addEventListener('mouseover', function(event) {
+        if(event.buttons === 1){
+            isHovering = true;
+        }
+
+        if(isHovering) {
+            const gridItems = event.target;
+            const randomColor = getRandomColor();
+            gridItems.style.backgroundColor = randomColor;
+        }
+    });
+
+    gridItems.addEventListener('mouseout', function() {
+        isHovering = false;
+        
+    })
+
+
+
+    container.appendChild(gridItems);
+}
+
+
+
 
